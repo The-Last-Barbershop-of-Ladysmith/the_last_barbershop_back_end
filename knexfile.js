eb8dccc4b030030e68ab21b16de72c469ae08671
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-05-02 22:10:16 -0400

    update knex config to use local postgresql db

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,16 @@
 const path = require('path');
 require('dotenv').config();
 
-const {
-  DATABASE_URL_DEVELOPMENT
-} = process.env;
-
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: DATABASE_URL_DEVELOPMENT,
+    connection: {
+      host : 'localhost',
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DEV
+    },
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -18,20 +19,20 @@ module.exports = {
     },
   },
 
-  staging: {
+  test: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host : 'localhost',
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_TEST
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
+    },
   },
 
   production: {
