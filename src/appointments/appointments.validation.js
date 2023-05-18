33f68aa7f03101328c854b92544e25cbeb31e2e0
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-05-18 18:39:16 -0400

    add date format validation for appointment date

--- src/appointments/appointments.validation.js
+++ src/appointments/appointments.validation.js
@@ -35,7 +35,16 @@ function validateMobileNumberFormat(req, res, next){
         : next({ status: 400, message: stringConstants.INVALID_MOBILE_NUMBER})
 }
 
+function validateDateFormat(req, res, next){
+    const { data: { appointment_date} } = req.body
+    const dateRegEx = /^[2-9]{1}\d{3}-(0[1-9]||1[0-2])-([0-2]{1}[0-9]{1}|3[0-1]{1})$/
+    const formatIsValid = dateRegEx.test(appointment_date)
+    formatIsValid
+        ? next()
+        : next({ status: 400, message: stringConstants.INVALID_DATE})
+}
+
 
 module.exports = {
-    create: [hasRequiredProperties, hasValidFields, validateMobileNumberFormat]
+    create: [hasRequiredProperties, hasValidFields, validateMobileNumberFormat, validateDateFormat]
 }
--- src/utils/string-constants.js
+++ src/utils/string-constants.js
@@ -1,6 +1,7 @@
 const constants = {
     APPOINTMENTS_TABLE: 'appointments',
-    INVALID_MOBILE_NUMBER: 'mobile_number must be in format 555-555-5555'
+    INVALID_MOBILE_NUMBER: 'mobile_number must be in format 555-555-5555',
+    INVALID_DATE: 'appointment_date must be in format YYYY-MM-DD'
 }
 
 module.exports = constants
