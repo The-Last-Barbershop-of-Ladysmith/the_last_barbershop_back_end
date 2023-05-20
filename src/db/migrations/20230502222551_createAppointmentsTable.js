8ee2e4c545fdf17ba00ea5a4404d576fb0d9ce2c
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-05-20 13:10:26 -0400

    update appointments table to generate uuid as primary id

--- src/db/migrations/20230502222551_createAppointmentsTable.js
+++ src/db/migrations/20230502222551_createAppointmentsTable.js
@@ -4,7 +4,7 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable("appointments", (table) => {
-      table.increments("appointment_id").primary();
+      table.uuid("appointment_id").primary().defaultTo(knex.raw('uuid_generate_v4()'));
       table.string("first_name").notNullable();
       table.string("last_name").notNullable();
       table.string("mobile_number").notNullable();
