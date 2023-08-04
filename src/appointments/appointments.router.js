9d9101640b630a57660c09dc2a1281076e56170c
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-08-04 15:09:27 -0400

    add request body validation to check a data property exists and throw bad request if it does not

--- src/appointments/appointments.router.js
+++ src/appointments/appointments.router.js
@@ -1,11 +1,10 @@
 const methodNotAllowed = require('../errors/methodNotAllowed')
 const router = require('express').Router();
 const controller = require('./appointments.controller');
-const validate = require('./appointments.validation')
 
 router
   .route('/')
-  .post(validate.create, controller.create)
+  .post(controller.create)
   .all(methodNotAllowed);
 
 module.exports = router
